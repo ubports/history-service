1095e81079dc99169705eb181eb4f812a6bc27f1
Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
2013-07-24 18:51:28 -0300

    Remove leftover debug print.

--- plugins/sqlite/sqlitehistoryeventview.cpp
+++ plugins/sqlite/sqlitehistoryeventview.cpp
@@ -66,7 +66,6 @@ SQLiteHistoryEventView::SQLiteHistoryEventView(SQLiteHistoryReader *reader,
         break;
     }
 
-    qDebug() << "Query text:" << queryText;
     if (!mQuery.exec(queryText)) {
         qCritical() << "Error:" << mQuery.lastError() << mQuery.lastQuery();
         return;
