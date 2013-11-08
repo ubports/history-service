cc5e93610322612c87b426689c2aea58c51d2a3f
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2013-11-08 12:55:13 -0200

    fix copyright

--- src/tests/PhoneUtilsTest.cpp
+++ src/tests/PhoneUtilsTest.cpp
@@ -3,11 +3,11 @@
  *
  * This file is part of history-service.
  *
- * telephony-service is free software; you can redistribute it and/or modify
+ * history-service is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
  *
- * telephony-service is distributed in the hope that it will be useful,
+ * history-service is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
