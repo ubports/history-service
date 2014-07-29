3ad66a32eba52a02618ffa0e33518347236b9913
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2014-07-29 15:11:22 -0300

    fix unreadCount update in triggers

---
+++ plugins/sqlite/schema/v8.sql
@@ -0,0 +1,71 @@
+DROP TRIGGER text_events_insert_trigger;
+DROP TRIGGER text_events_update_trigger;
+DROP TRIGGER text_events_delete_trigger;
+
+CREATE TRIGGER text_events_insert_trigger  AFTER INSERT ON text_events
+FOR EACH ROW
+BEGIN
+    UPDATE threads SET count=(SELECT count(eventId) FROM text_events WHERE
+        accountId=new.accountId AND
+        threadId=new.threadId)
+        WHERE accountId=new.accountId AND threadId=new.threadId AND type=0;
+    UPDATE threads SET unreadCount=(SELECT count(eventId) FROM text_events WHERE
+        accountId=new.accountId AND threadId=new.threadId AND newEvent='1')
+        WHERE accountId=new.accountId AND threadId=new.threadId AND type=0;
+    UPDATE threads SET lastEventId=(SELECT eventId FROM text_events WHERE
+        accountId=new.accountId AND
+        threadId=new.threadId
+        ORDER BY timestamp DESC LIMIT 1)
+        WHERE accountId=new.accountId AND threadId=new.threadId AND type=0;
+    UPDATE threads SET lastEventTimestamp=(SELECT timestamp FROM text_events WHERE
+        accountId=new.accountId AND
+        threadId=new.threadId
+        ORDER BY timestamp DESC LIMIT 1)
+        WHERE accountId=new.accountId AND threadId=new.threadId AND type=0;
+END;
+CREATE TRIGGER text_events_update_trigger  AFTER UPDATE ON text_events
+FOR EACH ROW
+BEGIN
+    UPDATE threads SET count=(SELECT count(eventId) FROM text_events WHERE
+        accountId=new.accountId AND
+        threadId=new.threadId)
+        WHERE accountId=new.accountId AND threadId=new.threadId AND type=0;
+    UPDATE threads SET unreadCount=(SELECT count(eventId) FROM text_events WHERE
+        accountId=new.accountId AND threadId=new.threadId AND newEvent='1')
+        WHERE accountId=new.accountId AND threadId=new.threadId AND type=0;
+    UPDATE threads SET lastEventId=(SELECT eventId FROM text_events WHERE
+        accountId=new.accountId AND
+        threadId=new.threadId
+        ORDER BY timestamp DESC LIMIT 1)
+        WHERE accountId=new.accountId AND threadId=new.threadId AND type=0;
+    UPDATE threads SET lastEventTimestamp=(SELECT timestamp FROM text_events WHERE
+        accountId=new.accountId AND
+        threadId=new.threadId
+        ORDER BY timestamp DESC LIMIT 1)
+        WHERE accountId=new.accountId AND threadId=new.threadId AND type=0;
+END;
+CREATE TRIGGER text_events_delete_trigger  AFTER DELETE ON text_events
+FOR EACH ROW
+BEGIN
+    UPDATE threads SET count=(SELECT count(eventId) FROM text_events WHERE
+        accountId=old.accountId AND
+        threadId=old.threadId)
+        WHERE accountId=old.accountId AND threadId=old.threadId AND type=0;
+    UPDATE threads SET unreadCount=(SELECT count(eventId) FROM text_events WHERE
+        accountId=old.accountId AND threadId=old.threadId AND newEvent='1')
+        WHERE accountId=old.accountId AND threadId=old.threadId AND type=0;
+    UPDATE threads SET lastEventId=(SELECT eventId FROM text_events WHERE
+        accountId=old.accountId AND
+        threadId=old.threadId
+        ORDER BY timestamp DESC LIMIT 1)
+        WHERE accountId=old.accountId AND threadId=old.threadId AND type=0;
+    UPDATE threads SET lastEventTimestamp=(SELECT timestamp FROM text_events WHERE
+        accountId=old.accountId AND
+        threadId=old.threadId
+        ORDER BY timestamp DESC LIMIT 1)
+        WHERE accountId=old.accountId AND threadId=old.threadId AND type=0;
+    DELETE from text_event_attachments WHERE
+        accountId=old.accountId AND
+        threadId=old.threadId AND
+        eventId=old.eventId;
+END;
