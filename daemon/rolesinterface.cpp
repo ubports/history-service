34a97e3091a07c13682bd0d50f99f76b7847e0c7
Roberto Mier Escandon <roberto.escandon@canonical.com>
2016-09-22 16:50:22 +0200

    added copyright for new files

--- daemon/rolesinterface.cpp
+++ daemon/rolesinterface.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2016 Canonical, Ltd.
+ *
+ * Authors:
+ *  Roberto Mier Escandon <roberto.escandon@canonical.com>
+ *
+ * This file is part of history-service.
+ *
+ * history-service is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * history-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <daemon/rolesinterface.h>
 
 ChannelInterfaceRolesInterface::ChannelInterfaceRolesInterface(const QString& busName, const QString& objectPath, QObject *parent)
--- daemon/rolesinterface.h
+++ daemon/rolesinterface.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2016 Canonical, Ltd.
+ *
+ * Authors:
+ *  Roberto Mier Escandon <roberto.escandon@canonical.com>
+ *
+ * This file is part of history-service.
+ *
+ * history-service is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * history-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef CHANNELINTERFACEROLESINTERFACE_H
 #define CHANNELINTERFACEROLESINTERFACE_H
 
@@ -193,8 +214,6 @@ Q_SIGNALS:
      *     map of handles and related roles removed
      */
      void RolesChanged(const HandleRolesMap &added, const HandleRolesMap& removed);
-
-     void CanUpdateRolesChanged(bool);
 
 protected:
     virtual void invalidate(Tp::DBusProxy *, const QString &, const QString &);
