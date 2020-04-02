1f06e2a893623fd3c40201ac6c845f3b97e791a8
Lionel Duboeuf <lionel.duboeuf@actility.com>
2020-04-02 11:43:08 +0200

    try to have xvfb output error

--- tests/xvfb_wrapper.sh
+++ tests/xvfb_wrapper.sh
@@ -1,6 +1,6 @@
 LOG=xvfb.err
 echo Running in virtual frame buffer...
-/usr/bin/xvfb-run "-e" "$LOG" "-a" "-s" "-screen 0 640x480x24" "/usr/bin/dbus-test-runner" "--keep-env" "--dbus-config=/home/lduboeuf/history-service/obj-arm-linux-gnueabihf/tests/common/dbus-session.conf" "--max-wait=5" "--task" "/home/lduboeuf/history-service/obj-arm-linux-gnueabihf/daemon/history-daemon" "--ignore-return" "--task-name" "history-daemon" "--task" "/home/lduboeuf/history-service/obj-arm-linux-gnueabihf/tests/Ubuntu.History/HistoryGroupedThreadsModelTest" "-p" "-o" "-p" "-,txt" "-p" "-o" "-p" "/home/lduboeuf/history-service/obj-arm-linux-gnueabihf/test_HistoryGroupedThreadsModelTest.xml,xunitxml" "--wait-for" "com.canonical.HistoryService" "--task-name" "HistoryGroupedThreadsModelTest" 2>$LOG
+/usr/bin/xvfb-run "-e" "$LOG" "-a" "-s" "-screen 0 640x480x24" "/usr/bin/dbus-test-runner" "--keep-env" "--dbus-config=$@/tests/common/dbus-session.conf" "--max-wait=5" "--task" "$@/daemon/history-daemon" "--ignore-return" "--task-name" "history-daemon" "--task" "$@/tests/Ubuntu.History/HistoryGroupedThreadsModelTest" "-p" "-o" "-p" "-,txt" "-p" "-o" "-p" "$@/test_HistoryGroupedThreadsModelTest.xml,xunitxml" "--wait-for" "com.canonical.HistoryService" "--task-name" "HistoryGroupedThreadsModelTest" 2>$LOG
 RETVAL=$?
 if [ $RETVAL -eq 0 ]; then
     echo $@ finished successfully...
