7c366c4101b487b45637421d5c2e2e8441de82a4
Lionel Duboeuf <lionel.duboeuf@actility.com>
2020-04-02 12:34:29 +0200

    i should stop spending all my time and having office work not done, but i can't ...

--- tests/xvfb_wrapper.sh
+++ tests/xvfb_wrapper.sh
@@ -1,6 +1,8 @@
 LOG=xvfb.err
+WAITFORX="tests/Xwait"
 echo Running in virtual frame buffer...
-/usr/bin/xvfb-run "-e" "$LOG" "-a" "-s" "-screen 0 640x480x24" "/usr/bin/dbus-test-runner" "--keep-env" "--dbus-config=$@/tests/common/dbus-session.conf" "--task" "$@/daemon/history-daemon" "--ignore-return" "--task-name" "history-daemon" "--task" "$@/tests/Ubuntu.History/HistoryGroupedThreadsModelTest" "-p" "-o" "-p" "-,txt" "-p" "-o" "-p" "$@/test_HistoryGroupedThreadsModelTest.xml,xunitxml" "--wait-for" "com.canonical.HistoryService" "--task-name" "HistoryGroupedThreadsModelTest" 2>$LOG
+
+/usr/bin/xvfb-run "-e" "$LOG" "-a" "-s" "-screen 0 640x480x24" ${WAITFORX} "/usr/bin/dbus-test-runner" "--keep-env" "--dbus-config=$@/tests/common/dbus-session.conf" "--task" "$@/daemon/history-daemon" "--ignore-return" "--task-name" "history-daemon" "--task" "$@/tests/Ubuntu.History/HistoryGroupedThreadsModelTest" "-p" "-o" "-p" "-,txt" "-p" "-o" "-p" "$@/test_HistoryGroupedThreadsModelTest.xml,xunitxml" "--wait-for" "com.canonical.HistoryService" "--task-name" "HistoryGroupedThreadsModelTest"  2>$LOG
 RETVAL=$?
 if [ $RETVAL -eq 0 ]; then
     echo $@ finished successfully...
