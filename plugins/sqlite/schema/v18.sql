401f1d5e890bd032fce98af32214983d93b9d46d
Lionel Duboeuf <lduboeuf@ouvaton.org>
2021-05-25 17:11:24 +0200

    protect sql update from failing if triggers already exists

--- plugins/sqlite/schema/v18.sql
+++ plugins/sqlite/schema/v18.sql
@@ -1,11 +1,11 @@
-CREATE TRIGGER text_threads_delete_trigger AFTER DELETE ON threads
+CREATE TRIGGER IF NOT EXISTS text_threads_delete_trigger AFTER DELETE ON threads
 FOR EACH ROW WHEN old.type=0
 BEGIN
     DELETE FROM text_events WHERE
         accountId=old.accountId AND
         threadId=old.threadId;
 END;
-CREATE TRIGGER voice_threads_delete_trigger AFTER DELETE ON threads
+CREATE TRIGGER IF NOT EXISTS voice_threads_delete_trigger AFTER DELETE ON threads
 FOR EACH ROW WHEN old.type=1
 BEGIN
     DELETE FROM voice_events WHERE
